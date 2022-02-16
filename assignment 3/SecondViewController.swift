91bde0455e38b63fbbf9134cb72d781435573a33
Duyi Li <li.duy@northeastern.edu>
2022-02-15 21:00:36 -0800

    assignment 3 part1

---
+++ assignment 3/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  assignment 3
+//
+//  Created by Duyi Li on 2/15/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var lblWelcome: UILabel!
+    var welcomeStr = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lblWelcome.text = welcomeStr
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func goBack(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
